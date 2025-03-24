07b7f52c47b013fef244716af6015af7ccc118c2
Jatin Kansal <jatin.kansal@oriserve.com>
2025-03-24 19:13:48 +0530

    changes

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.json())
 
 app.use('/ai', aiRoutes)
 
-app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')))
+app.use(express.static(path.resolve(__dirname, 'frontend', 'build')))
+app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
   
