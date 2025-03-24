13f4d1690b5e3036892a789a5bd3cd888cf35c6c
Jatin Kansal <jatin.kansal@oriserve.com>
2025-03-24 18:23:50 +0530

    conifg change

--- app.js
+++ app.js
@@ -12,9 +12,12 @@ app.use(express.json())
 
 app.use('/ai', aiRoutes)
 
-app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
-});
+app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+})
+  
+app.get('/', (req, res) => {
+    res.send('Welcome to ai code assistant app')
+})
 module.exports = app
