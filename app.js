1f756e64686624170f1384380687bcb7cc14119e
Jatin Kansal <jatin.kansal@oriserve.com>
2025-05-04 20:21:11 +0530

    route update for frontend build

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use(express.json())
 
 app.use('/ai', aiRoutes)
 
-app.use(express.static(path.resolve(__dirname, 'frontend', 'dist')))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-})
+app.get('/', (req, res) => {
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
   
 app.get('/', (req, res) => {
     res.send('Welcome to ai code assistant app')
